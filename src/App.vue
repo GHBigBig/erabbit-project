1cbeca2122b07571f8001cae33c00af812e156e3
yoga <876466053@qq.com>
2022-11-17 10:31:33 +0800

    项目基本搭建完成

---
+++ src/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>小兔儿鲜</h1>
+</template>
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  state() {
+    return {
+      count: 0,
+    };
+  },
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
+
+export default store;
