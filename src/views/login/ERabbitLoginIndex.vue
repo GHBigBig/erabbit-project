6b1415ba6cdbb704c9ece23f044be97f87fa544a
yoga <876466053@qq.com>
2022-12-27 19:43:33 +0800

    登录页 基础布局

--- src/views/login/ERabbitLoginIndex.vue
+++ src/views/login/ERabbitLoginIndex.vue
@@ -3,7 +3,68 @@ export default {
   name: 'ERabbitLoginIndex',
 };
 </script>
+<script setup>
+import ERabbitLoginFooter from './components/ERabbitLoginFooter.vue';
+import ERabbitLoginHeader from './components/ERabbitLoginHeader.vue';
+</script>
 <template>
-  <div class="page-login">Login</div>
+  <div class="page-login">
+    <ERabbitLoginHeader></ERabbitLoginHeader>
+    <main class="login-main">
+      <div class="wrapper">
+        <nav>
+          <ul>
+            <li><a href="javascript:;" class="active">账户登录</a></li>
+            <li><a href="javascript:;">扫码登录</a></li>
+          </ul>
+        </nav>
+      </div>
+    </main>
+    <ERabbitLoginFooter></ERabbitLoginFooter>
+  </div>
 </template>
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.login-main {
+  height: 488px;
+  background: url(../../assets/images/login-bg.png) no-repeat center/ cover;
+  position: relative;
+  .wrapper {
+    width: 380px;
+    min-height: 400px;
+    position: absolute;
+    left: 50%;
+    top: 54px;
+    transform: translate3d(100px, 0, 0);
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+    background-color: #fff;
+    nav {
+      height: 55px;
+      padding: 0 40px;
+      border-bottom: 1px solid #f5f5f5;
+      ul {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        li {
+          flex: 1;
+          text-align: center;
+          &:first-child {
+            border-right: 1px solid #f5f5f5;
+          }
+          a {
+            line-height: 1;
+            font-size: 18px;
+            display: inline-block;
+            &.active {
+              color: @erColor;
+              font-weight: bold;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/login/components/ERabbitLoginHeader.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+  name: 'ERabbitLoginHeader',
+};
+</script>
+
+<template>
+  <header class="login-header">
+    <div class="container">
+      <h1 class="logo"><RouterLink to="/">小兔鲜儿</RouterLink></h1>
+      <h3 class="sub"><slot>欢迎登录</slot></h3>
+      <RouterLink class="entry" to="/">
+        进入网站首页
+        <i class="iconfont icon-angle-right"></i>
+        <i class="iconfont icon-angle-right"></i>
+      </RouterLink>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="less">
+.login-header {
+  background-color: #fff;
+  border-bottom: 1px solid #e4e4e4;
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .logo {
+      width: 200px;
+      a {
+        width: 100%;
+        height: 132px;
+        display: block;
+        text-indent: -9999px;
+        background: url(../../../assets/images/logo.png) no-repeat center 18px /
+          contain;
+      }
+    }
+    .sub {
+      font-size: 24px;
+      font-weight: normal;
+      color: #666;
+    }
+    .entry {
+      width: 200px;
+      font-size: 16px;
+      i {
+        font-size: 14px;
+        color: @erColor;
+        letter-spacing: -5px;
+      }
+    }
+  }
+}
+</style>
